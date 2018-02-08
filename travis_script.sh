866ac28b4f08eddc4eda0145e95711263d21b865
haegeldd <haegele.david@gmail.com>
2018-02-08 18:00:45 +0100

    lets see if travis can do this now

--- travis_script.sh
+++ travis_script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-./generate_float_code.sh
+echo "script: executing generate_float_code.sh"
+sh ./generate_float_code.sh
+echo "script: executing maven test"
 mvn -P travis -X -pl :ezfftw clean test jacoco:report
