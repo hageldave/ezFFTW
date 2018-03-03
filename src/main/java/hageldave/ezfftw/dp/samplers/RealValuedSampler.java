11d8fd3a0abb4c8255cac2f04b9d0eff58d7366a
David <david@RECHENKNECHTLNX>
2018-03-03 01:38:08 +0100

    javadocs for RealValuedArraySampler and RowMajorArraySampler

--- src/main/java/hageldave/ezfftw/dp/samplers/RealValuedSampler.java
+++ src/main/java/hageldave/ezfftw/dp/samplers/RealValuedSampler.java
@@ -2,10 +2,34 @@ package hageldave.ezfftw.dp.samplers;
 
 import hageldave.ezfftw.Annotations.DoublePrecisionVersion;
 
+/**
+ * The RealValuedSampler interface is a single method interface which provides the
+ * {@link #getValueAt(long...)} method for sampling an arbitrary dimensional
+ * real domain.
+ * <p>
+ * It also offers a default convenience method for adding an imaginary component
+ * which yields a {@link ComplexValuedSampler}.
+ * 
+ * @author hageldave
+ *
+ */
 @DoublePrecisionVersion
 public interface RealValuedSampler {
+	
+	/**
+	 * Returns the value at the specified coordinates.
+	 * @param coordinates each value corresponds to one dimension
+	 * @return value at specified coords
+	 */
 	double getValueAt(long... coordinates);
 
+	/**
+	 * Combines this RealValuedSampler with another to form a {@link ComplexValuedSampler}.
+	 * This sampler will return the real parts of the complex values, the argument
+	 * sampler will return the imaginary parts.
+	 * @param imaginarySampler responsible for returning the imaginary values
+	 * @return ComplexValuedSampler consisting of this and the argument sampler
+	 */
 	default ComplexValuedSampler addImaginaryComponent(RealValuedSampler imaginarySampler){
 		RealValuedSampler self = this;
 		return (imaginary, coordinates) -> {
--- src/main/java/hageldave/ezfftw/dp/samplers/RowMajorArraySampler.java
+++ src/main/java/hageldave/ezfftw/dp/samplers/RowMajorArraySampler.java
@@ -1,21 +1,75 @@
 package hageldave.ezfftw.dp.samplers;
 
 import hageldave.ezfftw.GeneralUtils;
+import hageldave.ezfftw.dp.PrecisionDependentUtils;
+import hageldave.ezfftw.dp.writers.RowMajorArrayWriter;
+
+import java.util.Arrays;
+
 import hageldave.ezfftw.Annotations.DoublePrecisionVersion;
 
+/**
+ * The RowMajorArraySampler implements the {@link RealValuedSampler} interface
+ * for sampling arbitrary dimensional data that is stored in a double[] in
+ * row major order.
+ * <p>
+ * This class mainly exists to serve as an example implementation for the
+ * RealValuedSampler interface.
+ * <p>
+ * Please note that for performance reasons, no checks are made to the fitness
+ * of the coordinates passed to {@link #getValueAt(long...)}. When using this
+ * class, it has to be made sure that only appropriate coordinates will be
+ * used with it (correct dimensions here: {@link #getDimensions()}).
+ * 
+ * @author hageldave
+ * 
+ * @see RowMajorArrayWriter
+ *
+ */
 @DoublePrecisionVersion
 public class RowMajorArraySampler implements RealValuedSampler {
 
+	/** the values for sampling */
 	public final double[] array;
 	private final long[] dimensions;
 
+	/**
+	 * Creates a new RowMajorArraySampler that uses the specified double[] assuming
+	 * row major order and the specified dimensions.
+	 * 
+	 * @param array of values in row major order
+	 * @param dimensions of the value array
+	 * 
+	 * @throws IllegalArgumentException <br>
+	 * when no dimensions were provided <br>
+	 * when one of the dimensions is not positive <br>
+	 * when the length of the supplied arrays do not match the number of elements resulting from specified dimensions
+	 * @throws NullPointerException if any of the specified arrays is null.
+	 */
 	public RowMajorArraySampler(double[] array, long... dimensions) {
+		GeneralUtils.requirePositive(dimensions.length, ()->"No dimensions were specified, need to pass at least 1 dimension");
+		GeneralUtils.requirePosititveDimensions(dimensions);
+		long numElements = GeneralUtils.numElementsFromDimensions(dimensions);
+		PrecisionDependentUtils.sanityCheckArray(array, numElements, "double[]");
+		
 		this.array = array;
 		this.dimensions = dimensions;
 	}
 
+	/**
+	 * Calculates the row major index for the specified coordinates using the dimensions of this sampler,
+	 * and gets the value.
+	 */
 	@Override
 	public double getValueAt(long... coordinates) {
 		return array[(int)GeneralUtils.indexFromCoordinates(coordinates, dimensions)];
+	}
+	
+	/**
+	 * Returns a copy of this RowMajorArraySamplers dimensions.
+	 * @return dimensions
+	 */
+	public long[] getDimensions() {
+		return Arrays.copyOf(dimensions, dimensions.length);
 	}
 }
